a81084228404a2b1f6ab0532fcbce5b74a6c2348
Iacopo Spalletti <i.spalletti@nephila.it>
2014-03-10 15:36:19 +0100

    Add demo project

---
+++ demo/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import *
+from django.conf.urls.i18n import i18n_patterns
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib import admin
+from django.conf import settings
+
+admin.autodiscover()
+
+urlpatterns = i18n_patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('cms.urls')),
+)
+
+if settings.DEBUG:
+    urlpatterns = patterns('',
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+) + staticfiles_urlpatterns() + urlpatterns
